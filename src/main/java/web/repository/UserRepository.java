ca0c1f5a1844af668f3c0aaac50b6a919c7453e8
Eugene Dlg <stayerx@gmail.com>
2023-11-27 19:39:38 +0300

    Ready for 1st review

--- src/main/java/web/repository/UserRepository.java
+++ src/main/java/web/repository/UserRepository.java
@@ -3,17 +3,6 @@ package web.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import web.model.User;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
-//    public String getUser(Integer id);
-//
-//    public List<User> getUsers();
-//
-//    public void createUser(User user);
-//
-//    public void updateUser(User user);
-//
-//    public void deleteUser(User user);
 
 }
